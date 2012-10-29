56120150a09c2d97314a3ec2179ed8ae599815a7
Andres Garcia Infante <andres.garcia.infante@gmail.com>
2012-10-29 18:29:59 -0300

    Preguntas finales agregadas

--- js/cuestionario.js
+++ js/cuestionario.js
@@ -1,25 +1,65 @@
 	var preguntas = [];
 	var respuestas = [];
 	var pregunta = 1;
+	var contador = 1;
+	var opcion = 0;
 	$(document).on("ready", function(){
 		$.getJSON('js/preguntas.json', function(data){
-		  	$.each(data.preguntas, function(key, value) {		
+			
+			// extrae y despliega las preguntas y alternativas que se encuentran en preguntas.json
+		  	$.each(data.preguntas, function(key, value) {	
+
 				preguntas.push('<p class="tituloPregunta">' + value['pregunta'] +'</p>');
 				preguntas.push('<ul class="respuestas">');
+				
+				contador = 1;
+				opcion = 0;	
+				if($(value['opcion']).length){
+					opcion = value['opcion'];
+				}
+				
 				$.each(value['respuestas'], function(key, value) {
-					preguntas.push('<li>' + value +'</li>');
-				});
+					if( contador++ != opcion){
+						preguntas.push('<li>' + value +'</li>');
+					}
+					else{
+						preguntas.push('<li class="opcion">' + value +'</li>');
+					}		
+				});		
 				preguntas.push('</ul>');
 				
-				$('<div/>', {
+				if($(value['preguntaFormulario']).length){
+					/*$.each(value['preguntaFormulario'], function(key, value) {*/
+						preguntas.push('<div class="anexo">');
+						preguntas.push('<p class="preguntaFormulario">' + value['preguntaFormulario'] +'</p>');
+						preguntas.push('<input type="text" class="respuestaFormulario" />');
+						preguntas.push('</div>');
+					//});
+				}
+				if($(value['subPregunta']).length){
+					$.each(value['subPregunta'], function(key, value) {
+						preguntas.push('<div class="anexo">');
+						preguntas.push('<p class="preguntaFormulario">' + value['pregunta'] +'</p>');
+						preguntas.push('<ul class="respuestas">');
+						$.each(value['respuestas'], function(key, value) {
+							preguntas.push('<li>' + value +'</li>');
+						});
+						preguntas.push('</ul>');
+						preguntas.push('</div>');
+					});
+				}		
+				$('<div>', {
 					'id': 'pregunta' + pregunta++,
     				'class': 'pregunta',
     				html: preguntas.join('')
   				}).appendTo('.preguntas');
 				preguntas = [];
 		  	});
-			pregunta = 1;
 			
+			
+			
+			// botones
+			pregunta = 1;
 			$('.preguntas').append('<div class="anterior">Anterior</div>');
 			$('.preguntas').append('<div class="siguiente">Siguiente</div>');
 			$('.preguntas').append('<div class="finalizar">Finalizar</div>');
@@ -41,9 +81,27 @@
 				});
 			});
 			
+			$('.opcion').on("click",function(){
+				$('.anexo').show();
+					
+				$(this).parent().children('li').removeClass('selected');
+				$(this).addClass('selected');
+				$('.cuestionario').css('height',$('#pregunta'+pregunta).height()+100);
+				event.preventDefault();
+			});
+			
+			
+			$(".respuestaFormulario").keyup(function(event){
+  				if($(this).val().length)
+					$('.siguiente').show();
+				else
+					$('.siguiente').hide();
+			});
+
 			$('.pregunta li').on("click",function(){
 				$(this).parent().children('li').removeClass('selected');
 				$(this).addClass('selected');
+				
 				if( pregunta != $('div.pregunta').size()){
 					pregunta++;
 					cargarPregunta(pregunta);
@@ -54,18 +112,17 @@
 			pregunta =1;
 			cargarPregunta(pregunta);
 			
-			
-			
-		});/* getJSON*/
+		});// #getJSON
 		
-	}); // ready
+	});// #ready
 	
 	function cargarPregunta(pregunta){
 		$('.pregunta').hide();
 		$('.anterior').show();
 		$('.siguiente').hide();
 		$('.finalizar').hide();
-
+		$('.anexo').hide();
+		
 		$('#pregunta'+pregunta).show();
 		$('#numeroPreguntas').text(pregunta + '/'+ $('.pregunta').size());
 		$('.cuestionario').css('height',$('#pregunta'+pregunta).height()+100);
@@ -80,7 +137,13 @@
 				$('.siguiente').show();
 			}
 		}
+		if($('.opcion'+ ' li.selected').length > 0){
+			$('.anexo').show();
+		}
 		
-
+		if($('#pregunta'+ pregunta + '.respuestaFormulario').val().length)
+					$('.siguiente').show();
+				else
+					$('.siguiente').hide();
 	}//cargarPregunta
 	
